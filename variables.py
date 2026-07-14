b1a9fddde94d249c21050dd4b3e4fc8f0af38408
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-14 21:04:09 +0530

    variables

--- variables.py
+++ variables.py
@@ -1,6 +1,27 @@
 # VARIABLES --> A CONTAINER FOR A VALUE (STRING , INTEGER , FLOAT , BOOLEAN) 
 # A VARIABLES BEHAVES AS IF IT WAS THE VALUE IT CONTAINS.....
 
-
+#string
 first_name = "udayan singh"
+food = "chilla"
+email = "udyan@g.com"
+
+#integer
+age = 19
+
+#float
+price = 99.99
+
+#boolean
+is_student = True
+sale = False
+
+
 print(f"hello {first_name}")
+print(f"i like {food }")
+print(f"my email is: {email}")
+print(f"my age is {age}")
+print(f"book price is {price}")
+print(f"are you a student {is_student}")
+print(f"is this item for sell {sale}")
+15:14
