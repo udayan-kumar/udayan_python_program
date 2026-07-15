eecfd09e897bb78c4c8ff273346ab21831b71410
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-15 21:04:31 +0530

    arithmetic operator

---
+++ arithmetic_operator.py
@@ -0,0 +1,22 @@
+age = 20
+
+age = age + 1
+age += 1
+
+age = age - 1
+age -= 1
+
+age = age * 10
+age *= 10
+
+age = age / 10
+age /= 10
+
+#EXPONENT
+age = age ** 2
+age **= 2
+
+age = age % 2
+age %= 2
+
+print(age)
