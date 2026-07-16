551976caaec59b1fb7de44b5deb9bcad32084825
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-16 12:39:24 +0530

    built in method

---
+++ built_in_function.py
@@ -0,0 +1,45 @@
+x = 3.14
+y = -4
+z = 8
+
+#ROUND
+round = round(x)
+print(round)
+
+#ABSOLUTE
+abs = abs(y)
+print(abs)
+
+#POWER
+power = pow(4,3)
+print(power)
+
+#MAXIMUM
+max = max(4,8)
+print(max)
+
+#MINIMUM
+min = min(5,8)
+print(min)
+
+#IMPORT MATH
+import math
+
+print(math.pi)
+print(math.e)
+
+print(math.sqrt(9))
+
+print(math.ceil(9.4))
+print(math.floor(9.4))
+
+
+
+# CALCULATE THE CIRCUMFERENCE OF CIRCLE
+
+radius = float(input())
+circumference = 2 * math.pi * float(radius)
+print(f"circumference of circle is {circumference}")
+
+area = math.pi * (radius * radius)
+print(f"are of circle is {area}")
