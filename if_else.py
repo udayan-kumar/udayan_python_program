4abd04f1376d45c6fb9598428f12d7ecc37fe609
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-16 12:47:02 +0530

    if else statement

---
+++ if_else.py
@@ -0,0 +1,11 @@
+# IF --> DO SOME CODE IF SOME CONDITION ID TRUE
+#           ELSE DO SOMETHING ELSE
+
+age = int(input())
+
+if age > 18:
+    print("you are eligible for giving vote")
+elif age < 9:
+        print("update your age")
+else: 
+    print("you are not ready")
