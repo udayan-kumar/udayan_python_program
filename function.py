28ecccace82d866ca0769bfbab8d683de6ed2b06
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-17 18:42:23 +0530

    function

---
+++ function.py
@@ -0,0 +1,10 @@
+# FUNCTON --> A BLOCK OF REUSABLE CODE
+#    PLACE () AFTER THE FUNCTION NAME TO INVOKE IT 
+
+def birthday(name , age):
+    print(f"happy birthday to {name}")
+    print(f"you are {age} year old")
+    print("happy birthday to you")
+    print()
+
+birthday("udayan singh" , 18)
