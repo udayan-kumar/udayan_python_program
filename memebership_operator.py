48f2029456016deede8095a9ec91808eb9d99930
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-18 01:28:33 +0530

    memebership operator

---
+++ memebership_operator.py
@@ -0,0 +1,18 @@
+#  MEMEBERSHIP OPERSTOR --> USED TO TEST WHETHER A VALUE OR VARIABLE IS FOUND IN A SEQUENCES (STRING , LIST , TUPLE , SET , OR DICTIONARY)
+#       1. IN
+#       2. NOT IN
+
+word = "apple"
+
+letter = input("guess a letter in  the seceret: ")
+
+# if letter in word:
+#     print(f"there is a {letter}")
+# else:
+#     print(f"{letter} was not found")
+
+if letter not in word:
+    print(f"{letter} was not found")
+   
+else:
+     print(f"there is a {letter}")
