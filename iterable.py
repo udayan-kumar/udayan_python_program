b09dae152a60748c88b5507317bdfcc60fd88c0a
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-17 22:38:48 +0530

    iterable

---
+++ iterable.py
@@ -0,0 +1,25 @@
+ # ITERABLE --> AN OBJECT/COLLECTIONS THAT CAN RETURN ITS ELEMENT ONE AT A TIME ..... ALLOWING IT TO THE ITERABLE OVER IN A LOOP
+
+# numbers = [1,2,3,4,5,6,7,8]
+numbers = (1,2,3,4,5,6,7,8)
+
+# for num in numbers:
+#     print(num , end=" ")
+
+# print()
+
+# for num in reversed(numbers):
+#     print(num , end=" ")
+
+
+# fruits = {"apple" , "orange" , "coconut"}  # not revered
+
+# for name in fruits:
+#     print(name , end=" ")
+
+name = "udayan singh"
+
+for char in name:
+    print(char , end= " ")
+
+4:37
