294248d9e9b0ca76292eb637d55abdccffe3d088
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-17 17:04:11 +0530

    list set tuple

---
+++ list_set_tuple.py
@@ -0,0 +1,36 @@
+# COLLECTION --> SINGLE "VARIABLE" USED TO STORE MULTIPLE VALUES 
+
+# LIST --> [] ORDERED AND CHANGEBALE .. DUBLICATES OK
+
+# SET --> {} UNORDERED AND IMMUTABLE , BUT ADD/REMOVE OK .. NO DUBLICATES
+
+# TUPLE --> () ORDERED AND UNCHANEBALE .. DUBLICATE OK .. FASTER
+
+fruits = ["apple", "orange", "banana"]  # list
+# fruits = ("apple", "orange", "banana")  # tupple
+# fruits = {"apple", "orange", "banana"}  # set
+
+# print((dir(fruits)))  # for checking attribute
+
+# print(fruits)
+# print(fruits[2])
+# print(len(fruits))
+# print("apple" in fruits)  # boolean type
+
+# fruits[1] = "udayan singh"
+# print(fruits)
+
+# for fruits in fruits:
+#     print(fruits)
+
+# fruits.append("udayan singh")
+# print(fruits)
+
+# fruits.remove("udayan singh")
+# print(fruits)
+
+# fruits.insert(0, "udayan singh")
+# print(fruits)
+
+# fruits.sort()
+# print(fruits)
