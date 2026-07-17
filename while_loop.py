76f354414114b51166352880be53f1f541de30df
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-17 16:11:17 +0530

    while loop

---
+++ while_loop.py
@@ -0,0 +1,9 @@
+# WHILE LOOP --> EXECUTE SOME CODE WHILE SOME CONDITION REMAINS TRUE
+
+num = int(input("enter your number: "))
+
+while num < 1 or num > 10:
+    print(f"{num} is not valid")
+    num = int(input("enter your number: "))
+
+    print(f"your number is: {num}")
