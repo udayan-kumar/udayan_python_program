00346f291f3f9ad3d51175ae1b8852c116b8ad9f
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-17 15:54:31 +0530

    format specifier

---
+++ format_specifier.py
@@ -0,0 +1,9 @@
+# FORMAT SPECIFIER --> {VALUE : FLAGS} FORMAT A VALUE BASED ON WHAT FLAGS ARE INSERTED
+
+price1 = 3.12345
+price2 = -987.65
+price3 = 12.34
+
+print(f"price1 is: {price1: .2f}")
+print(f"price2 is: {price2}")
+print(f"price3 is: {price3}")
