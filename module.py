694a915112391c8fc9a2df70a3ffa1caef6c7f77
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-18 16:47:11 +0530

    module

---
+++ module.py
@@ -0,0 +1,9 @@
+# MODULE --> A FILE CONTAINING CODE YOU WANT TO INCLUDE IN YOUR PROGRAM USE "import" TO INCLUDE A MODULE (BUILT-IN OR YOUR OWN) USEFUL TO BREAK UP A LARGE PROGRAM REUSABLE SEPRATE FILES...
+
+# import math
+# import math as m
+from math import pi
+
+# print(math.pi)
+# print(m.pi)
+print(pi)
