712d86c9b9f5eeb4f9fc43ad853c96b869b2b15f
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-19 21:10:18 +0530

    reading file

---
+++ reading_file/reading_file.py
@@ -0,0 +1,25 @@
+# PYTHON READING FILE (.txt , .json , .csv)
+import json
+import csv
+
+# file_path = "D:/udayan_python_program/writing_file/output_txt"
+
+# file_path = "D:/udayan_python_program/writing_file/output_json"
+
+file_path = "D:/udayan_python_program/writing_file/output_csv"
+
+try:
+    with open(file_path , "r") as file:
+        # content = file.read()
+        # print(content)
+
+        # content = json.load(file)
+        # print(content)
+
+        content = csv.reader(file)
+        for line in content:
+            print(line)
+       
+except FileNotFoundError:
+    print("file doesn't found")
+    8:48
