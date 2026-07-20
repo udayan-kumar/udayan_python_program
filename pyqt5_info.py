e48fb42caaae2bebece164fcb026d3e9626c2cd3
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-20 07:41:14 +0530

    pyqt5 images

--- pyqt5_info.py
+++ pyqt5_info.py
@@ -1,11 +1,32 @@
 import sys
-from PyQt5.QtWidgets import QApplication , QMainWindow
+from PyQt5.QtWidgets import QApplication , QMainWindow , QLabel
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("my cool first GUI")
-        self.setGeometry(700 , 300 , 500 , 500)
+        self.setGeometry(0 , 0 , 500 , 500)
+
+        # label = QLabel("udayan singh" , self)
+
+        # label.setFont(QFont("Arial" , 30))
+
+        # label.setGeometry(0,0,500,100)
+
+        # label.setStyleSheet("color: blue;" "background-color:black;" "font-weight:bold;" "font-style:italic;" "text-decoration:underline")
+        # # label.setAlignment(Qt.AlignTop)
+        # # label.setAlignment(Qt.AlignBottom)
+        # label.setAlignment(Qt.AlignVCenter)
+
+        label = QLabel(self)
+        label.setGeometry(0,0,500,500)
+
+        pixmap = QPixmap("images.jpg")
+        label.setPixmap(pixmap)
+        label.setScaledContents(True)
 
 def main():
     app = QApplication(sys.argv)
@@ -16,4 +37,4 @@ def main():
 if __name__ == "__main__":
     main()
 
-    9:31
+  
