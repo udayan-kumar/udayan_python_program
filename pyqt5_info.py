ac8f72baf8680f7da7a5bf6221745a91df31c657
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-27 19:45:51 +0530

    pyqt5 info

--- pyqt5_info.py
+++ pyqt5_info.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication , QMainWindow , QLabel , QWidget , QVBoxLayout , QHBoxLayout , QGridLayout
+from PyQt5.QtWidgets import QApplication , QMainWindow , QLabel , QWidget , QVBoxLayout , QHBoxLayout , QGridLayout 
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
@@ -29,23 +29,23 @@ class MainWindow(QMainWindow):
         # label.setPixmap(pixmap)
         # label.setScaledContents(True)
 
-    def initUI(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+    # def initUI(self):
+    #     central_widget = QWidget()
+    #     self.setCentralWidget(central_widget)
 
-        label1 = QLabel("#1" , self)
-        label2 = QLabel("#2" , self)
-        label3 = QLabel("#3" , self)
-        label4 = QLabel("#4" , self)
-        label5 = QLabel("#5" , self)
+    #     label1 = QLabel("#1" , self)
+    #     label2 = QLabel("#2" , self)
+    #     label3 = QLabel("#3" , self)
+    #     label4 = QLabel("#4" , self)
+    #     label5 = QLabel("#5" , self)
 
-        label1.setStyleSheet("background-color: red")
-        label2.setStyleSheet("background-color: blue")
-        label3.setStyleSheet("background-color: black")
-        label4.setStyleSheet("background-color: orange")
-        label5.setStyleSheet("background-color: yellow")
+    #     label1.setStyleSheet("background-color: red")
+    #     label2.setStyleSheet("background-color: blue")
+    #     label3.setStyleSheet("background-color: black")
+    #     label4.setStyleSheet("background-color: orange")
+    #     label5.setStyleSheet("background-color: yellow")
 
-        # vbox = QVBoxLayout()
+    #     # vbox = QVBoxLayout()
 
         # vbox.addWidget(label1)
         # vbox.addWidget(label2)
@@ -61,26 +61,30 @@ class MainWindow(QMainWindow):
         # hbox.addWidget(label4)
         # hbox.addWidget(label5)
 
-        grid= QGridLayout()
+        # grid= QGridLayout()
 
-        grid.addWidget(label1, 0, 0)
-        grid.addWidget(label2, 0, 1)
-        grid.addWidget(label3, 1,0)
-        grid.addWidget(label4,1,1)
-        grid.addWidget(label5,1,2)
+        # grid.addWidget(label1, 0, 0)
+        # grid.addWidget(label2, 0, 1)
+        # grid.addWidget(label3, 1,0)
+        # grid.addWidget(label4,1,1)
+        # grid.addWidget(label5,1,2)
 
 
-        # central_widget.setLayout(vbox)
-        # central_widget.setLayout(hbox)
-        central_widget.setLayout(grid)
+        # # central_widget.setLayout(vbox)
+        # # central_widget.setLayout(hbox)
+        # central_widget.setLayout(grid)
 
-def main():
+        
+
+# def main():
+#     app = QApplication(sys.argv)
+#     window = MainWindow()
+#     window.show()
+#     sys.exit(app.exec_())
+
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
 
-if __name__ == "__main__":
-    main()
-
-9:53
